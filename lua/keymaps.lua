e5217571de20089efbca31baebbc040b4458c4a8
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-06 10:24:15 +0530

    feat: made ctrl-n open on right side

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -49,7 +49,7 @@ vim.api.nvim_set_keymap("n", "<C-W>.", ":vertical resize +10<CR>", { noremap = t
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- nvim tree
-vim.api.nvim_set_keymap("n", "<C-n>", "<cmd>Vex<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-n>", "<cmd>Vex!<CR>", { noremap = true })
 
 -- save
 vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>w<CR>", { noremap = true })
