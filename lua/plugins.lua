f457de55553048cc53fcf016fc4a9f3def30362f
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-26 10:40:52 +0530

    feat: added dressing ui and also synced packages

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -91,6 +91,23 @@ local plugins = {
         dependencies = { "nvim-lua/plenary.nvim" }
     },
 
+    -- better vim.ui
+    {
+        "stevearc/dressing.nvim",
+        lazy = true,
+        init = function()
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
 }
 
 local opts = {
