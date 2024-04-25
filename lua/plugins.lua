572255dc81dffb8a47faef3482e3b389a2b6fa7f
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-04-25 18:13:15 +0530

    feat: go plugin added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,6 +7,22 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
+    -- Go
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("go").setup()
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", 'gomod' },
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
+
     -- Theme
     {
         "folke/tokyonight.nvim",
