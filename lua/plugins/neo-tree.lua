72f632e42591f1f31665c0f9368609f02609078d
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-08-24 21:07:13 +0530

    fix: stopped opening of neotree on startup

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,28 @@
 return {
-    -- "nvim-neo-tree/neo-tree.nvim",
-    -- opts = {
-    --     reveal = true,
-    --     source_selector = {
-    --         winbar = false,
-    --         statusline = false
-    --     },
-    --     default_component_configs = {
-    --         name = {
-    --             use_git_status_colors = false
-    --         }
-    --     }
-    -- },
-    -- branch = "v3.x",
-    -- dependencies = {
-    --     "nvim-lua/plenary.nvim",
-    --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    --     "MunifTanjim/nui.nvim",
-    -- }
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        reveal = true,
+        source_selector = {
+            winbar = false,
+            statusline = false
+        },
+        default_component_configs = {
+            name = {
+                use_git_status_colors = false
+            }
+        },
+        filesystem = {
+            follow_current_file = {
+                enabled = true,
+                leave_dirs_open = true,
+            },
+            hijack_netrw_behavior = "disabled",
+        },
+    },
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    }
 }
