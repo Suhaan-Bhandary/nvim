88fec2d65c220145acd1f37c8ccbcde77c7fc391
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2025-11-27 13:20:39 +0530

    using snacks for select and input

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,9 +4,14 @@ return {
         ---@type snacks.Config
         opts = {
             picker = {
-                -- your picker configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
+            },
+            ui = {
+                select = {
+                    enabled = true
+                },
+                input = {
+                    enabled = true
+                }
             }
         }
     }
